b4e77c03f3d10ac24657c0268715942d26f7a3d7
Juan David Ortiz <davidaviddavid3@gmail.com>
2025-10-14 23:04:46 -0500

    Creación parcial del login y comunicación entre activities

--- app/src/main/java/com/ucompensar/project_store/activities/SplashActivity.kt
+++ app/src/main/java/com/ucompensar/project_store/activities/SplashActivity.kt
@@ -1,21 +1,29 @@
 package com.ucompensar.project_store.activities
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.ucompensar.project_store.MainActivity
 import com.ucompensar.project_store.R
 
 class SplashActivity : AppCompatActivity() {
+
+    private val SPLASH_DURATION = 3000L
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, SPLASH_DURATION)
     }
 }
