5bcaa4514fe35511cd614f1e1a501bd37da666db
Juan David Ortiz <davidaviddavid3@gmail.com>
2025-10-18 15:53:22 -0500

    Actización de icono y pantalla splash

--- app/src/main/java/com/ucompensar/project_store/activities/SplashActivity.kt
+++ app/src/main/java/com/ucompensar/project_store/activities/SplashActivity.kt
@@ -1,9 +1,13 @@
 package com.ucompensar.project_store.activities
 
 import android.content.Intent
+import android.graphics.Color
+import android.graphics.LinearGradient
+import android.graphics.Shader
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,6 +22,23 @@ class SplashActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
+
+        // dentro de onCreate
+        val textView = findViewById<TextView>(R.id.txt_splash_three)
+
+        // Creamos el degradado (de arriba a abajo)
+        val textShader = LinearGradient(
+            0f, 0f, 0f, textView.textSize * textView.lineCount,
+            intArrayOf(
+                Color.parseColor("#53E88B"), // Color arriba (verde claro)
+                Color.parseColor("#15BE77")  // Color abajo (verde oscuro)
+            ),
+            null,
+            Shader.TileMode.CLAMP
+        )
+
+        // Asignamos el degradado al texto
+        textView.paint.shader = textShader
 
         Handler(Looper.getMainLooper()).postDelayed({
             val intent = Intent(this, LoginActivity::class.java)
