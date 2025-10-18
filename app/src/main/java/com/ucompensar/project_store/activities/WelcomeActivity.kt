73ab75c815dfaa7fed09bb4eef87b507f6cc7737
Juan David Ortiz <davidaviddavid3@gmail.com>
2025-10-18 17:33:16 -0500

    Agregar INTENT en el botón entrar de WELCOME

--- app/src/main/java/com/ucompensar/project_store/activities/WelcomeActivity.kt
+++ app/src/main/java/com/ucompensar/project_store/activities/WelcomeActivity.kt
@@ -1,9 +1,11 @@
 package com.ucompensar.project_store.activities
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.LinearGradient
 import android.graphics.Shader
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,22 +19,12 @@ class WelcomeActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_welcome)
 
-//        // dentro de onCreate
-//        val textView = findViewById<TextView>(R.id.txt_welcome)
-//
-//        // Creamos el degradado (de arriba a abajo)
-//        val textShader = LinearGradient(
-//            0f, 0f, 0f, textView.textSize * textView.lineCount,
-//            intArrayOf(
-//                Color.parseColor("#53E88B"), // Color arriba (verde claro)
-//                Color.parseColor("#15BE77")  // Color abajo (verde oscuro)
-//            ),
-//            null,
-//            Shader.TileMode.CLAMP
-//        )
-//
-//        // Asignamos el degradado al texto
-//        textView.paint.shader = textShader
+        val btn_entrar: Button = findViewById(R.id.btn_entrar)
 
+        btn_entrar.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
